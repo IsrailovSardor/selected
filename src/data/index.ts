62545b90904df6da7f23f42fa9217b2fb1eecf0d
Sardor <sardor_09062002@mail.ru>
2023-03-20 23:49:59 +0600

    feature/created selected

---
+++ src/data/index.ts
@@ -0,0 +1,55 @@
+import ru from '../assets/media/flags/ru.svg'
+import en from '../assets/media/flags/en.svg'
+import es from '../assets/media/flags/es.svg'
+import it from '../assets/media/flags/it.svg'
+import nz from '../assets/media/flags/nz.svg'
+import pl from '../assets/media/flags/pl.svg'
+
+
+export const countriesList = [
+    {   
+        id:0,
+        logo: ru,
+        title: 'Русский'
+    },
+    {   
+        id:1,
+        logo: en,
+        title: 'Английский'
+    },
+    {   
+        id:2,
+        logo: es,
+        title: 'Испанский'
+    },
+    {   
+        id:3,
+        logo: nz,
+        title: 'Немецкий'
+    },
+    {   
+        id:4,
+        logo: it,
+        title: 'Итальянский'
+    },
+    {   
+        id:5,
+        logo: pl,
+        title: 'Польский'
+    },
+    {   
+        id:6,
+        logo: nz,
+        title: 'Немецкий'
+    },
+    {   
+        id:7,
+        logo: it,
+        title: 'Итальянский'
+    },
+    {   
+        id:8,
+        logo: pl,
+        title: 'Польский'
+    },
+]
